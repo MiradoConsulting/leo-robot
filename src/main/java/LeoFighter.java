7d413717396e3b7d4b00443216255fc628031870
leonid.minaiev <leonid.minaiev@mirado.com>
2024-04-05 18:42:48 +0200

    Leo robot is alive

--- src/main/java/LeoFighter.java
+++ src/main/java/LeoFighter.java
@@ -1,7 +1,7 @@
 import robocode.*;
-import robocode.Robot;
 
 import java.awt.Color;
+import java.util.Random;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
@@ -24,10 +24,11 @@ public class LeoFighter extends Robot
         // Robot main loop
         while(true) {
             // Replace the next 4 lines with any behavior you would like
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+            ahead(20);
+            turnGunRight(20);
+            ahead(20);
+            turnLeft(30);
+            ahead(new Random(100).nextLong());
         }
     }
 
@@ -36,7 +37,7 @@ public class LeoFighter extends Robot
      */
     public void onScannedRobot(ScannedRobotEvent e) {
         // Replace the next line with any behavior you would like
-        fire(1);
+        fire(10);
     }
 
     /**
@@ -44,7 +45,8 @@ public class LeoFighter extends Robot
      */
     public void onHitByBullet(HitByBulletEvent e) {
         // Replace the next line with any behavior you would like
-        back(10);
+        turnLeft(10);
+        ahead(40);
     }
 
     /**
@@ -52,6 +54,6 @@ public class LeoFighter extends Robot
      */
     public void onHitWall(HitWallEvent e) {
         // Replace the next line with any behavior you would like
-        back(20);
+        turnLeft(30);
     }
 }
