6c50443c6bc5d361d5f84b03da6edb54259703fb
leonid.minaiev <leonid.minaiev@mirado.com>
2024-04-05 19:25:09 +0200

    Leo robot is alive

--- src/main/java/LeoFighter.java
+++ src/main/java/LeoFighter.java
@@ -19,55 +19,45 @@ public class LeoFighter extends Robot
         // After trying out your robot, try uncommenting the import at the top,
         // and the next line:
 
-         setColors(Color.yellow,Color.blue,Color.pink); // body,gun,radar
+         setColors(Color.green,Color.blue,Color.red); // body,gun,radar
 
         // Robot main loop
         while(true) {
             // Replace the next 4 lines with any behavior you would like
-            ahead(100); //Go ahead 100 pixels
-            turnGunRight(360); //scan
-            back(75); //Go back 75 pixels
-            turnGunRight(360); //scan
+            ahead(20);
+            turnGunRight(20);
+            ahead(20);
+            turnLeft(30);
+            ahead(new Random(100).nextLong());
         }
     }
 
     /**
      * onScannedRobot: What to do when you see another robot
      */
-    public void onScannedRobot(ScannedRobotEvent e){
-        double distance = e.getDistance(); //get the distance of the scanned robot
-        if(distance > 800) //this conditions adjust the fire force according the distance of the scanned robot.
-            fire(5);
-        else if(distance > 600 && distance <= 800)
-            fire(4);
-        else if(distance > 400 && distance <= 600)
-            fire(3);
-        else if(distance > 200 && distance <= 400)
-            fire(2);
-        else if(distance < 200)
-            fire(1);
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Replace the next line with any behavior you would like
+        fire(3);
+        turnGunLeft(3);
+        fire(1);
+        turnGunRight(8);
+        fire(1);
     }
 
     /**
      * onHitByBullet: What to do when you're hit by a bullet
      */
-
-    public void onHitByBullet(HitByBulletEvent e){
-        double energy = getEnergy();
-        double bearing = e.getBearing(); //Get the direction which is arrived the bullet.
-        if(energy < 100){ // if the energy is low, the robot go away from the enemy
-            turnRight(-bearing); //This isn't accurate but release your robot.
-            ahead(100); //The robot goes away from the enemy.
-        }
-        else
-            turnRight(360); // scan
+    public void onHitByBullet(HitByBulletEvent e) {
+        // Replace the next line with any behavior you would like
+        turnLeft(15);
+        ahead(50);
     }
+
     /**
      * onHitWall: What to do when you hit a wall
      */
-    public void onHitWall(HitWallEvent e){
-        double bearing = e.getBearing(); //get the bearing of the wall
-        turnRight(-bearing); //This isn't accurate but release your robot.
-        ahead(100); //The robot goes away from the wall.
+    public void onHitWall(HitWallEvent e) {
+        // Replace the next line with any behavior you would like
+        turnLeft(30);
     }
 }
